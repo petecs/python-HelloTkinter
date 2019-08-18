c4007d496176a0d2ec62b0b721bf905ce6fb8f0a
Peter Akala <40690295+petecs@users.noreply.github.com>
2019-08-18 10:35:57 -0700

    added messagebox popups and text entry box

--- hello_tkinter.py
+++ hello_tkinter.py
@@ -1,26 +1,39 @@
 import tkinter
+import tkinter.messagebox
 
 class Window:
     def __init__(self):
         self.main_window = tkinter.Tk()
         self.main_window.title("Hello Tkinter")
 
-        self.label = tkinter.Label(self.main_window, text = "Choose One")
-        self.label.pack(fill=tkinter.BOTH, expand=True, padx=100, pady=30)
+        self.label_text = tkinter.StringVar()
+        self.label_text.set("My name is: ")
 
-        hello_button = tkinter.Button(self.main_window, text="Say Hello", command=self.say_hello)
-        hello_button.pack(side=tkinter.LEFT, padx=(20,0), pady=(0,20))
+        self.name_text = tkinter.StringVar()
 
-        goodbye_button = tkinter.Button(self.main_window, text="Say Goodbye", command=self.say_goodbye)
-        goodbye_button.pack(side=tkinter.RIGHT, padx=(0,20), pady=(0,20))
+        self.label = tkinter.Label(self.main_window, textvariable=self.label_text)
+        self.label.pack(fill=tkinter.BOTH, expand=True, padx=100, pady=10)
+
+        self.name_entry = tkinter.Entry(self.main_window, textvariable=self.name_text)
+        self.name_entry.pack(fill=tkinter.BOTH, expand=True, padx=20, pady=20)
+
+        self.hello_button = tkinter.Button(self.main_window, text="Say Hello", command=self.say_hello)
+        self.hello_button.pack(side=tkinter.LEFT, padx=(20,0), pady=(0,20))
+
+        self.goodbye_button = tkinter.Button(self.main_window, text="Say Goodbye", command=self.say_goodbye)
+        self.goodbye_button.pack(side=tkinter.RIGHT, padx=(0,20), pady=(0,20))
     
     def say_hello(self):
-        self.label.configure(text="Hello World!")
+        message = "Hello there " + self.name_entry.get()
+        tkinter.messagebox.showinfo("Hello", message)
 
     def say_goodbye(self):
-        self.label.configure(text="Goodbye!\n (Closing in 2 seconds)")
-        self.main_window.after(2000, self.main_window.destroy)
-
+        if tkinter.messagebox.askyesno("Close Window?", "Would you like to close this window?"):
+            message = "Window will close in 2 seconds - goodbye " + self.name_entry.get()
+            self.label_text.set(message)
+            self.main_window.after(2000, self.main_window.destroy)
+        else:
+            tkinter.messagebox.showinfo("Not Closing", "Great! This window will stay open.")
 
 def main():
     root = Window()
